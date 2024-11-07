77c1e9fe774f1f55949afbada2d79d8779d36b54
SirJuiceBox <123216149+SirJuiceBox@users.noreply.github.com>
2024-11-07 13:43:26 -0700

    Update Lab3.m

--- Lab3.m
+++ Lab3.m
@@ -1,10 +1,12 @@
-y_t = (t*c/0.2)*((0.2969*sqrt(x/c))-(0.126*(x/c))-(0.3516*((x/c)^2))+0.2843*((x/c)^3))-(0.1036*((x/c)^4));
+NACA = '4415'; % INPUT NUMBER ONLY
 
-%y_c1 = @(x) m
+m = str2num(NACA(1))/100;
+p = str2num(NACA(2))/10;
+t = str2num(NACA(3))/10 + str2num(NACA(4))/100;
 
-dy_c1dx = 2*m*(c*p-x)/c/(p^2);
-dy_c2dx = (2*m*(c*p-x)) / (c*((1-p)^2));
+y_t = @(t,c,x) (t*c/0.2)((0.2969*sqrt(x/c))-(0.126*(x/c))-(0.3516*((x/c)^2))+0.2843*((x/c)^3))-(0.1036*((x/c)^4));
 
+dy_c1-dx = @(x) 2*m*(c*p-x)/c/(p^2);
+dy_c2-dx = @(x) (2*m*(c*p-x)) / (c*((1-p)^2));
 
-zeta1 = atan(dy_c1dx);
-zeta2 = atan(dy_c2dx);
+zeta = 
